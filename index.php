e4f203973cd7fdec99071be5aa020167592a48f7
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2022-03-22 11:18:47 +0100

    include pages header and head in home page

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Covid-test</title>
-    <link rel="stylesheet" href="./css/bootstrap.css">
-    <link rel="stylesheet" href="./css/style.css">
-</head>
+<?php
+include('components/head.php');
+?>
+
 <body>
+    <?php
+    include('components/header.php');
+    ?>
     <div class="bg-warning text-center">
         <p class="fw-bold h4">LA DERNIÈRE INFO MÉDICALE</p>
         <p class="text-primary">
