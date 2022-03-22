b595b2bb717f1ed7d899ca06f4cfd65c5ccbe676
lahlaliwassim2 <wlahlali34@gmail.com>
2022-03-22 14:21:06 +0100

    creation de la premiere partie de la page conseilles

---
+++ conseils.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include('components/head.php')
+?>
+<body>
+            <div class="container">
+                <div class="row">
+                    <div class="mt-4 col-12 text-center">
+                        <h1 class="w-h1" >Comment se protéger et protéger les autres au quotidien ?</h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class=" col-sm-6 w-card col-md-4">
+                        <img class="w-img" src="assets/img/ico3.svg" alt="">
+                        <h5 class="w-h5">Saluez sans serrer la main, n'embrassez personne</h5>
+                    </div>
+                    <div class="col-12 col-sm-6 w-card col-md-4">
+                        <img class="w-img" src="assets/img/ico1.svg" alt="">
+                        <h5 class="w-h5">Saluez sans serrer la main, n'embrassez personne</h5>
+                    </div>
+                    <div class="col-12 col-sm-6 w-card col-md-4">
+                        <img class="w-img" src="assets/img/ico2.svg" alt="">
+                        <h5 class="w-h5">Saluez sans serrer la main, n'embrassez personne</h5>
+                    </div>
+                    <div class="col-12 col-sm-6 w-card col-md-4">
+                        <img class="w-img" src="assets/img/ico6.svg" alt="">
+                        <h5 class="w-h5">Saluez sans serrer la main, n'embrassez personne</h5>
+                    </div>
+                    <div class="col-12 col-sm-6 w-card col-md-4">
+                        <img class="w-img" src="assets/img/ico5.svg" alt="">
+                        <h5 class="w-h5">Saluez sans serrer la main, n'embrassez personne</h5>
+                    </div>
+                    <div class="col-12 col-sm-6 w-card col-md-4">
+                        <img class="w-img" src="assets/img/ico4.svg" alt="">
+                        <h5 class="w-h5">Saluez sans serrer la main, n'embrassez personne</h5>
+                    </div>
+                </div>
+                <div class="row p-30 mt-30 mt-md-5" ></div>
+                    <div class="mx-auto text-center ">
+                    <img class="w-img" src="assets/img/icon-house.svg" alt="">
+                        <h5 class="w-h5">Restez chez vous, évitez tout contact avec les autres</h5>
+                    </div>
+            </div>
+            <section id="infor" >
+            <div class="container-fluid border ">
+                <div class="col-sm-12">
+                    <h2 class="w-h text-center">informations</h2>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-sm-12 col-lg-12 relative text-center" >
+                    <p class="absolute w-para">Obtenez des informations à jour en provenance du Ministère de la Santé</p>
+                </div>
+                        <div class="text-center">
+                        <button type="button" class="btn btn-primary btn-lg">www.covidmaroc.ma</button>
+                        </div>
+                 </div>
+            </div>
+        </section>
+</body>
+</html>
+
