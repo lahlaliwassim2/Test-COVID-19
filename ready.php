9d3ba578b2c213f7ff962e8653e16d360e94579c
Mohamed Laghabr <93931981+Mohamed-Laghbar@users.noreply.github.com>
2022-03-22 14:50:20 +0100

    structure de page d'information

---
+++ ready.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include './components/head.php'; ?>
+
+<body>
+    <?php include './components/header.php'; ?>
+    <div class="container-fluid">
+        <div class="pagination">
+            <div class="stiper d-flex justify-content-around my-3">
+                <div class="Info d-flex flex-column align-items-center">
+                    <span class=" circle rounded-circle"></span>
+                    <p>Informations</p>
+                </div>
+                <div class="Ques d-flex flex-column align-items-center">
+                    <span class=" circle rounded-circle"></span>
+                    <p>Questionnaire</p>
+                </div>
+                <div class="Res d-flex flex-column align-items-center">
+                    <span class=" circle rounded-circle"></span>
+                    <p>Résulats</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
