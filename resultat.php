4cc36275d4a84c5f28eea7aba1c15786a7ce359c
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2022-03-27 17:59:11 +0000

    ajouter style de page activer

--- resultat.php
+++ resultat.php
@@ -12,11 +12,13 @@
                 <p>Informations</p>
             </div>
             <div class="Ques d-flex flex-column align-items-center">
-                <span class=" circle rounded-circle"></span>
+                <span class="circle rounded-circle"></span>
                 <p>Questionnaire</p>
             </div>
             <div class="Res d-flex flex-column align-items-center">
-                <span class=" circle rounded-circle"></span>
+                <div class="circleSelct rounded-circle">
+                    <div class="circle rounded-circle circleSelc"></div>
+                </div>
                 <p>Résulats</p>
             </div>
         </div>
